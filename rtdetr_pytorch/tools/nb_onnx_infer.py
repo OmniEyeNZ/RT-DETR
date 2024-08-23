4f321aeec0c345eb66f5046899bf45073ebd2ca0
Bin Wang <bin.wang@herd-i.com>
2024-08-23 20:51:16 +1200

    feat: develop the rt-detr inference

--- rtdetr_pytorch/tools/nb_onnx_infer.py
+++ rtdetr_pytorch/tools/nb_onnx_infer.py
@@ -18,8 +18,8 @@ img_path = "/home/irisdc01/training/dataset/detection-images/nz.fol_id.as-7775-0
 # %%
 # Define functions
 #%%
-## Inference function
-def infer(session, image):
+## Output the box information through init the picture and run model
+def model_infer(session, image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (640, 640))
     image = image.transpose(2, 0, 1)
@@ -44,98 +44,108 @@ def postprocess(outputs, score_threshold):
     return filtered_boxes, filtered_labels, filtered_scores
 
 #%%
-## Simple version of a tracker class
-class SimpleTracker:
+# Record boxes that match the scores in one picture
+class BoxSet:
     def __init__(self):
-        self.tracks = []
+        self.boxes = []
 
-    def update(self, boxes, labels, scores):
-        new_tracks = []
+    def update(self, boxes, labels, scores, actual_high=1080, actual_width=1920):
+        new_boxes = []
         for box, label, score in zip(boxes, labels, scores):
-            track = {'box': box, 'label': label, 'score': score}
-            new_tracks.append(track)
-        self.tracks = new_tracks
+            box = box.astype(int)
+            box[0] = int(box[0] * (actual_width / 640))
+            box[1] = int(box[1] * (actual_high / 640))
+            box[2] = int(box[2] * (actual_width / 640))
+            box[3] = int(box[3] * (actual_high / 640))
+            #print(type(box), ":", box)
+            box_info = {'box': box, 'label': label, 'score': score}
+            new_boxes.append(box_info)
+        self.boxes = new_boxes
 
+    def clear(self):
+        self.boxes = []
 
 #%%
-## Function to draw boxes on an image
-def drawBoxInImage(img_path, model_path=MODEL_PATH, ouput_path=OUTPUT_TMP_FOLDER+"output.jpg", score_threshold=SCORE_THRESHOLD):
-    frame = cv2.imread(img_path)
-    # inference
-    session = ort.InferenceSession(model_path)
-    outputs = infer(session, frame)
-    r_width = frame.shape[1]
-    r_high = frame.shape[0]
+# Inference the picture and return all boxes
+def inference(session, frame, box_set: BoxSet, score_threshold=SCORE_THRESHOLD) -> BoxSet:
+    outputs = model_infer(session, frame)
+    actual_high = frame.shape[0]
+    actual_width = frame.shape[1]
+
     # post processing
     boxes, labels, scores = postprocess(outputs, score_threshold)
-    # update track
-    tracker = SimpleTracker()
-    tracker.update(boxes, labels, scores)
+    # update box_set
+    box_set.update(boxes, labels, scores, actual_high, actual_width)
+
+    return box_set
+#%%
+## Function to draw boxes on an image
+def drawBoxInImage(img_path, model_path=MODEL_PATH, ouput_path=OUTPUT_TMP_FOLDER+"output.jpg"):
+    session = ort.InferenceSession(MODEL_PATH)
+    frame = cv2.imread(img_path)
+    box_set = BoxSet()
+    box_set_out = inference(session, frame, box_set)  # get all boxes
     # output
-    for track in tracker.tracks:
+    for box_info in box_set_out.boxes:
         # x1:wid  y1:high
-        x1, y1, x2, y2 = map(int, track['box'])
-        x1 = int(x1 * (r_width / 640))
-        y1 = int(y1 * (r_high / 640))
-        x2 = int(x2 * (r_width / 640))
-        y2 = int(y2 * (r_high / 640))
-        print("=========", x1, y1, x2, y2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        x1, y1, x2, y2 = map(int, box_info['box'])
+        #print("=========", x1, y1, x2, y2, "-----", track['score'])
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
         cv2.putText(
             frame,
-            str(track['label']) + " : " + str('{:.2f}'.format(track['score'])),
+            str('{:.2f}'.format(box_info['score'])),
             (x1, y1),
             cv2.FONT_HERSHEY_SIMPLEX,
-            1,
+            2,
             (0, 0, 255),
-            2
+            3
         )
     cv2.imwrite(ouput_path, frame)
-
 #%%
 ## Function to draw boxes on video
-def drawBoxInVideo(video_path, model_path=MODEL_PATH, ouput_path=OUTPUT_TMP_FOLDER+"output.mp4", score_threshold=SCORE_THRESHOLD):
+def drawBoxInVideo(video_path, model_path=MODEL_PATH, ouput_path=OUTPUT_TMP_FOLDER+"output.mp4"):
     session = ort.InferenceSession(model_path)
-
-    tracker = SimpleTracker()
-
+    box_set = BoxSet()
     cap = cv2.VideoCapture(video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print(f"video_path:{video_path} {fps} {video_height} {video_width}")
+
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    size = (video_width, video_height)
+    video = cv2.VideoWriter(ouput_path, fourcc, fps, size)
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         # inference
-        outputs = infer(session, frame)
-        r_width = frame.shape[1]
-        r_high = frame.shape[0]
-        # post processing
-        boxes, labels, scores = postprocess(outputs, score_threshold)
-        # update track
-        tracker.update(boxes, labels, scores)
-        #print(tracker.tracks)
-        # config output vidoe
-        for track in tracker.tracks:
-            # print(track['box'])
-            # x1:wid  y1:high
-            x1, y1, x2, y2 = map(int, track['box'])
-            x1 = int(x1 * (r_width / 640))
-            y1 = int(y1 * (r_high / 640))
-            x2 = int(x2 * (r_width / 640))
-            y2 = int(y2 * (r_high / 640))
+        box_set_out = inference(session, frame, box_set)  # get all boxes
+        for box_info in box_set_out.boxes:
+            x1, y1, x2, y2 = map(int, box_info['box'])
             #print("=========", x1, y1, x2, y2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
             cv2.putText(
                 frame,
-                str(track['label']) + " : " + str('{:.2f}'.format(track['score'])),
+                str('{:.2f}'.format(box_info['score'])),
                 (x1, y1),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                1,
+                2,
                 (0, 0, 255),
-                2
+                3
             )
-
+        video.write(frame)
+        # cv2.imshow('Tracking', frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
     cap.release()
+    video.release()
+
 # %%
 # draw bounding boxes on the sample image
 drawBoxInImage(img_path)
+# %%
+# draw bounding boxes on the sample video
+drawBoxInVideo(video_path)
 # %%
